b42664f36c3e72220fa2d2b295ab12950bf0e174
Emir Uzunovic <emir.uzunovic@gmail.com>
2018-12-08 21:21:42 +0100

    JSON response added

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 
-//adding event listener to a button
+// event listener on button
 function randomIndex(){
-    //to generate random index for array
-    let RandomIndex = Math.floor(Math.random()*BoxOfquotes.quotes.length);
-    document.getElementById("text").innerHTML = BoxOfquotes.quotes[RandomIndex].quote ;
-    document.getElementById("author").innerHTML = BoxOfquotes.quotes[RandomIndex].author;
-    document.getElementById("tweet-quote").setAttribute("href","twitter.com/intent/tweet" + BoxOfquotes.quotes[RandomIndex].quote);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json', true);
+   
+    xhr.onload = function(){
+        if(this.status === 200){
+            let boxOfQuotes = JSON.parse(this.responseText);
+            //to generate random index for array
+            let RandomIndex = Math.floor(Math.random()*boxOfQuotes.quotes.length);
+          
+            document.getElementById("text").innerHTML = boxOfQuotes.quotes[RandomIndex].quote ;
+            document.getElementById("author").innerHTML = boxOfQuotes.quotes[RandomIndex].author;
+            document.getElementById("tweet-quote").setAttribute("href","twitter.com/intent/tweet" + boxOfQuotes.quotes[RandomIndex].quote);
+
+        }
+         
+    }
+
+    //ON ERROR sta da radi ako mu error 404 izbaci tj ne nadje ono sta se poziva    
+    xhr.onerror = function(){
+        console.log("Request Error")
+    }
+  
+    // Sends request
+    xhr.send();
 }
